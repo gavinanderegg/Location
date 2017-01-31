0c2b9bde2c2a31080aac1ae29ee8e7edc3d50a77
Gavin Anderegg <gavin@anderegg.ca>
2017-01-30 21:19:48 -0400

    Location!

--- Location/ViewController.swift
+++ Location/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    let annotation = MKPointAnnotation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var position: UILabel!
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        annotation.coordinate = locValue
+        mapView.removeAnnotations([annotation])
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        position.text = "\(locValue.latitude), \(locValue.longitude)"
+    }
+    
 }
 
