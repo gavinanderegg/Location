adde5c3fde77bcc8539324373af02a2c05a70b93
Gavin Anderegg <gavin@anderegg.ca>
2017-01-30 20:05:20 -0400

    Initial Commit

---
+++ Location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by Gavin Anderegg on 2017-01-30.
+//  Copyright © 2017 Gavin Anderegg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
